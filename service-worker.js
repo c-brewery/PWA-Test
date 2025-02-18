5d767dfd1056f9607b72bfbeee9a88d71fb3d75b
c-brewery <christoph.brauers@gmail.com>
2025-02-18 19:06:09 +0100

    refactor: update manifest and improve service worker registration

--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,14 @@
-self.addEventListener('install', function(e) {
-    console.log('[Service Worker] Install');
+self.addEventListener('install', event => {
+  console.log('Service Worker installing.');
+  // Add a call to skipWaiting here if you want to activate the SW immediately
+  // self.skipWaiting();
 });
 
-var cacheName = 'v1';
-var appShellFiles = [
-    './',
-    './index.html',
-    './about.html',
-    './blog.html',
-];
-
-self.addEventListener('install', function(e) {
-    console.log('[Service Worker] Install');
-    e.waitUntil(
-        caches.open(cacheName).then(function(cache) {
-            console.log('[Service Worker] Caching all: app shell and content');
-            return cache.addAll(appShellFiles);
-        })
-    );
+self.addEventListener('activate', event => {
+  console.log('Service Worker activating.');
 });
 
-self.addEventListener('fetch', function(e) {
-    e.respondWith(
-        caches.match(e.request).then(function(r) {
-            console.log('[Service Worker] Fetching resource: '+e.request.url);
-            return r || fetch(e.request).then(function(response) {
-                return caches.open(cacheName).then(function(cache) {
-                    console.log('[Service Worker] Caching new resource: '+e.request.url);
-                    cache.put(e.request, response.clone());
-                    return response;
-                });
-            });
-        })
-    );
+self.addEventListener('fetch', event => {
+  console.log('Fetching:', event.request.url);
+  // You can add custom fetch event handling here
 });
