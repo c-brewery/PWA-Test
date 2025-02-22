64485eab9623c714d16b3800ca94f3e5b9f91f22
c-brewery <christoph.brauers@gmail.com>
2025-02-22 16:38:31 +0100

    feat: implement dropdown menu for enhanced file operations, improve user interaction and accessibility

--- script.js
+++ script.js
@@ -197,3 +197,20 @@ document.addEventListener('DOMContentLoaded', () => {
   document.body.appendChild(readerContainer);
   document.body.appendChild(qrCodeResultContainer);
 });
+
+function toggleDropdown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    const dropdowns = document.getElementsByClassName("dropdown-content");
+    for (let i = 0; i < dropdowns.length; i++) {
+      const openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
